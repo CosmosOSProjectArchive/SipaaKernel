16713ae9b4ee5aa2f3d21a6d84cef238d286ee47
RaphMar2019 M <raphaelmartinaud@hotmail.com>
2022-11-15 20:27:11 +0100

    Disabled setup, Added a TextBlock control

---
+++ SipaaKernel.UI/Widgets/TextBlock.cs
@@ -0,0 +1,18 @@
+﻿using PrismGL2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SipaaKernel.UI.SysTheme2.ThemeManager;
+namespace SipaaKernel.UI.Widgets
+{
+    // a text block is literally nothing lol
+    public class TextBlock : Widget
+    {
+        public override void OnDraw(Graphics g)
+        {
+            g.DrawString(X, Y, Text, Font.Fallback, GetCurrentTheme().GetForegroundColor());
+        }
+    }
+}
--- SipaaKernel/Kernel.cs
+++ SipaaKernel/Kernel.cs
@@ -32,10 +32,10 @@ namespace SipaaKernel
             Core.Global.Boot(false);
 
             // Check if SipaaKernel needs to be installed
-            if (!SipaaKernelInstallationManager.IsInstalled)
+            /**if (!SipaaKernelInstallationManager.IsInstalled)
             {
                 s = new();
-            }
+            }**/
 
             // Resize the wallpaper & Init a button
             Assets.Wallpaper = Assets.Wallpaper.Scale(VBE.getModeInfo().width, VBE.getModeInfo().height);
@@ -57,16 +57,8 @@ namespace SipaaKernel
         {
             //c.Clear(Color.Black);
             c.DrawImage(0, 0, Assets.Wallpaper, false);
-            if (s != null)
-            {
-                s.Draw(c);
-                s.Update(c);
-            }
-            else
-            {
-                b.OnDraw(c);
-                b.OnUpdate();
-            }
+            b.OnDraw(c);
+            b.OnUpdate();
             c.DrawFilledRectangle((int)Sys.MouseManager.X, (int)Sys.MouseManager.Y, 8, 12, 0, Color.White);
             c.DrawString(10, 10, $"{c.GetFPS()} FPS", Font.Fallback, Color.White);
             c.Update();
